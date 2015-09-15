d45b8a8bd75806aaa2a130659a269852c5ad31b0
Jura <jurazubach@gmail.com>
2015-09-15 16:57:02 +0300

    Update Application.php

--- Framework/Application.php
+++ Framework/Application.php
@@ -4,7 +4,29 @@
  * User: JuraZubach
  * Date: 11.09.15
  */
+namespace Framework\Application;
 
 class Application {
+
+    protected $config;
+
+    public function __construct($param)
+    {
+        if(file_exists($param) && is_readable($param)) {
+            $this->config = include($param);
+        }
+    }
+
+
+    public function run()
+    {
+        /**
+         * Created Request and Response
+         */
+        $request = new Request;
+        $response = new Response;
+
+
+    }
 
 }
