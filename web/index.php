959f11b79b0defed7cf01f6d3175809b5afbea0a
Jura <jurazubach@gmail.com>
2015-09-15 16:56:27 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -7,5 +7,5 @@ $loader->addNamespacePath('Blog\\',__DIR__.'/../src/Blog');
 $loader->addNamespacePath('Framework\\',__DIR__.'/../framework');
 $loader->register();
 
-$app = new \Framework\Application(__DIR__.'/../app/config/config.php');
-$app->run();
+$application = new \Framework\Application(__DIR__.'/../app/config/config.php');
+$application->run();
