4c10382ff0d703320dad59814bbd2ba62a63103f
Jura <jurazubach@gmail.com>
2015-09-26 16:00:14 +0300

    Update Service.php

--- Framework/DI/Service.php
+++ Framework/DI/Service.php
@@ -7,6 +7,33 @@
 
 namespace Framework\DI;
 
-class Service {
+class Service 
+{
+    private static $instance = null;
+    private $values = array();
 
+    private function __construct()
+    {
+
+    }
+
+    static function instanse()
+    {
+        if(is_null(self::$instance)){
+            self::$instance = new self();
+        }
+    }
+
+    function get($key)
+    {
+        if(isset($this->values[$key])){
+            return $this->values[$key];
+        }
+        return null;
+    }
+
+    function set ($key, $value)
+    {
+        $this->values[$key] = $value;
+    }
 }
