064b54db96ac1d4f86436c512fbe84f06ea0b7ba
Jura <jurazubach@gmail.com>
2015-09-27 20:33:12 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,11 +1,8 @@
 <?php
 
 require_once(__DIR__ . '/../Framework/Loader.php');
-
-$loader = Loader::getInstance();
 $loader->addNamespacePath('Blog\\',__DIR__.'/../src/Blog');
 $loader->addNamespacePath('Framework\\',__DIR__.'/../framework');
-$loader->register();
 
-$application = new \Framework\Application(__DIR__.'/../app/config/config.php');
-$application->run();
+$app = new \Framework\Application(__DIR__.'/../app/config/config.php');
+$app->run();
