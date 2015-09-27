82719207ce6961d84564f461ce1eda55cd57ad93
Jura <jurazubach@gmail.com>
2015-09-27 20:44:06 +0300

    Update Loader.php

--- Framework/Loader.php
+++ Framework/Loader.php
@@ -6,7 +6,7 @@ use Framework\Exception\ClassNotFoundException;
  * This is a class loader.
  *
  * @package Application
- * @autor Jura Zubach
+ * @author Jura Zubach
  * @since 1.0
  */
 class Loader {
