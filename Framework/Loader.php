a27aeeab8185f20873a86defb50d240d4891f4d0
Jura <jurazubach@gmail.com>
2015-09-27 20:32:48 +0300

    Update Loader.php

--- Framework/Loader.php
+++ Framework/Loader.php
@@ -1,89 +1,110 @@
 <?php
-/**
- * Description:
- * User: JuraZubach
- * Date: 11.09.15
- */
 
 use Framework\Exception\ClassNotFoundException;
 
+/**
+ * This is a class loader.
+ *
+ * @package Application
+ * @autor Jura Zubach
+ * @since 1.0
+ */
 class Loader {
 
-    /**
-     * @var object  Stored instance
-     */
-    protected static $_instance;
 
-    /**
-     * Map array
-     */
-    protected $namespacesMap = array();
+    private static $instance;
 
-    /**
-     * Constructor
-     */
+    private $namespaces = array();
+
     private function __construct()
     {
+        //...
     }
 
-    /**
-     * Cloning method
-     */
-    private function __clone(){}
+    private function __clone()
+    {
+        //...
+    }
 
-    /**
-     * @return object|Loader
-     */
     public static function getInstance()
     {
-        if(null === self::$_instance) {
-            self::$_instance = new self();
+        if(empty(self::$instance))
+        {
+            self::$instance = new self();
         }
-        return self::$_instance;
+        return self::$instance;
     }
 
-
+    /**
+     * @return array
+     */
     public function register()
     {
         spl_autoload_register(array($this,'loadClass'));
     }
 
-
+    /**
+     * @return array
+     */
     public function unregister()
     {
         spl_autoload_unregister(array($this, 'loadClass'));
     }
 
+    /**
+     * @param $instance
+     */
+    public static function setInstance($instance)
+    {
+        self::$instance = $instance;
+    }
 
+    /**
+     * @param $namespace
+     * @param $rootDir
+     * @return bool
+     */
     public function addNamespacePath($namespace, $rootDir)
     {
-        if (is_dir($rootDir)) {
+        if (is_dir($rootDir))
+        {
             $namespace = trim($namespace, '\\');
-            $this->namespacesMap[$namespace] = $rootDir;
+            $this->namespaces[$namespace] = $rootDir;
             return true;
         }
 
         return false;
     }
 
-
+    /**
+     * @param $class
+     * @return bool
+     * @throws ClassNotFoundException
+     */
     protected function loadClass($class)
     {
         $pathParts = explode('\\', $class);
-        if(is_array($pathParts)) {
+        if(is_array($pathParts))
+        {
             $namespace = array_shift($pathParts);
-            if (!empty($this->namespacesMap[$namespace])) {
-                $filePath = $this->namespacesMap[$namespace] . '/' . implode('/', $pathParts) . '.php';
-                if(file_exists($filePath)) {
+            if (!empty($this->namespaces[$namespace]))
+            {
+                $filePath = $this->namespaces[$namespace] . '/' . implode('/', $pathParts) . '.php';
+                if(file_exists($filePath))
+                {
                     require_once $filePath;
-                } else {
+                }else
+                {
                     throw new ClassNotFoundException($class.' not found in : '.$filePath);
-                }
-                return true;
-            } else {
-
+                }return true;
+            } else
+            {
+                //...}
             }
         }
         return false;
     }
 }
+
+$loader = Loader::getInstance();
+$loader->register();
