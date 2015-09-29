cc7c16aceb712323914656c0aa1ca000d0e18ac3
Jura <jurazubach@gmail.com>
2015-09-29 15:13:44 +0300

    Create ServiceNotFoundExeption.php

---
+++ Framework/Exception/ServiceNotFoundExeption.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Framework\Exception;
+
+/**
+ * Service not found exception.
+ *
+ * @package Exception
+ * @author Jura Zubach
+ * @since 1.0
+ */
+class ServiceNotFoundExeption extends FrameworkException
+{
+
+}
