235764c163bdfe2088ea58a93b1cd2920fa270b5
Jura <jurazubach@gmail.com>
2015-09-29 15:14:59 +0300

    Update Router.php

--- Framework/Router/Router.php
+++ Framework/Router/Router.php
@@ -1,12 +1,21 @@
 <?php
-/**
- * Description:
- * User: JuraZubach
- * Date: 11.09.15
- */
 
 namespace Framework\Router;
 
+/**
+ * Router.
+ *
+ * @package Router
+ * @author Jura Zubach
+ * @since 1.0
+ */
 class Router {
+
+    protected $map = array();
+
+    public function __construct($routing_map = array())
+    {
+        $this->map = $routing_map;
+    }
 
 }
