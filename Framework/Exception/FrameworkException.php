09b0896e9f3763f1bd667fef0183bb673d7106b5
Jura <jurazubach@gmail.com>
2015-10-06 13:10:53 +0300

    Update FrameworkException.php

--- Framework/Exception/FrameworkException.php
+++ Framework/Exception/FrameworkException.php
@@ -11,29 +11,5 @@ namespace Framework\Exception;
  */
 class FrameworkException extends \Exception
 {
-    private $statusCode;
-    private $headers;
-    
-    public function __construct($statusCode, $message = null, \Exception $previous = null, array $headers = array(), $code = 0)
-    {
-        $this->statusCode = $statusCode;
-        $this->headers = $headers;
-        parent::__construct($message, $code, $previous);
-    }
-
-    /**
-     * @return string
-     */
-    public function getStatusCode()
-    {
-        return $this->statusCode;
-    }
-
-    /**
-     * @return array
-     */
-    public function getHeaders()
-    {
-        return $this->headers;
-    }
+    //...
 }
