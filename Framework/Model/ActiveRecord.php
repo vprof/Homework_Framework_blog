5f63ff5615ba8855f5d6469b1cc016e7c353ca71
Jura <jurazubach@gmail.com>
2015-10-06 13:10:23 +0300

    Update ActiveRecord.php

--- Framework/Model/ActiveRecord.php
+++ Framework/Model/ActiveRecord.php
@@ -1,12 +1,45 @@
 <?php
-/**
- * Description:
- * User: JuraZubach
- * Date: 11.09.15
- */
 
 namespace Framework\Model;
 
-class ActiveRecord {
+/**
+ * ActiveRecord class.
+ *
+ * @package Exception
+ * @author Jura Zubach
+ * @since 1.0
+ */
+class ActiveRecord
+{
+
+    protected $table;
+    protected $key_field_name = 'id';
+
+
+    public function find()
+    {
+        //... command change
+    }
+
+    public function save()
+    {
+        $fields = get_object_vars($this);
+
+        $coulmns = '';// Get column names for $table;
+        foreach($coulmns as $col =>$value)
+        {
+            /*if( array_key_exists()){
+                $sql_part .= sprintf(' `%s`=%s'. $col, $fields[$col]);}
+            $sql .= 'WHERE' . $this->key_field_name.'='.$this-> ??? ;
+
+            implode(','. $sql_part);*/
+        }
+
+    }
+
+    public function getRules()
+    {
+
+    }
 
 }
