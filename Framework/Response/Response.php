7d202b8374a48f125a2769a5808217e41152aeeb
Jura <jurazubach@gmail.com>
2015-10-06 13:08:38 +0300

    Update Response.php

--- Framework/Response/Response.php
+++ Framework/Response/Response.php
@@ -1,12 +1,46 @@
 <?php
-/**
- * Description:
- * User: JuraZubach
- * Date: 11.09.15
- */
 
 namespace Framework\Response;
 
-class Response{
+use Framework\Response\ResponseInterface;
 
+class Response implements ResponseInterface{
+
+    protected $headers = array();
+    public $type = 'html';
+
+    protected $content;
+    protected $msg;
+    public $code;
+
+    public function __construct($content, $msg = '', $code = 200){
+
+        $this->content = $content;
+        $this->msg = $msg;
+        $this->code = $code;
+    }
+
+    function setHeader( $header )
+    {
+        $this->headers[] = $header;
+    }
+
+    function getContent()
+    {
+        return $this->content;
+    }
+
+    function getCode()
+    {
+        return $this->code;
+    }
+
+    function send()
+    {
+        $this->setHeader('HTTP/1.1 '.$this->code . ' ' . $this->msg);
+
+        header(implode("\n", $this->headers));
+
+        echo $this->getContent();
+    }
 }
